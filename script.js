17f4a3b4c4f25d004011bb1f8e98e337d7af2558
Norberto Contreras <88049567+Nordevelop@users.noreply.github.com>
2023-05-23 19:11:56 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+function mostrarmensaje(){
+    let mensaje = "Info Colecta maratea ";
+    alert(mensaje);//2
+let dinero =prompt("Que monto desea donar para salvar a Independiente de la B (de vuelta) " , 100);//seguir este patron
+if(dinero >=10000){
+    //10000 pesos
+    bienvenido();
+   function bienvenido(){
+      alert("Gracias por apoyar a Independiete te  ganaste una entrada para el Clasico Independiente VS El Porvenir ");
+    }
+}else{
+    bienvenido();
+    function bienvenido(){
+        alert("Pero que mishiaduro y pecho frio!!!!");
+    }
+}
+console.log("colecta");
+
+
+
 const allSideMenu = document.querySelectorAll('#sidebar .side-menu.top li a');
 
 allSideMenu.forEach(item=> {
